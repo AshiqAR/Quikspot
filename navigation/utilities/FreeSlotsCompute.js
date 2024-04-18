a8e6c4d7147954dd0fff704558fb468375c177e4
AshiqAR <ashiqkallara.cet@gmail.com>
2024-04-19 00:14:14 +0530

    availableSlot updation

--- navigation/utilities/FreeSlotsCompute.js
+++ navigation/utilities/FreeSlotsCompute.js
@@ -23,6 +23,58 @@ const getActiveBookingsCount = activeBookingsData => {
   return result.length;
 };
 
+const getCarBookings = activeBookingsData => {
+  console.log("Active bookings data: ", activeBookingsData);
+  const currentTime = Date.now();
+  const fiveMinutes = 5 * 60 * 1000; // Three minutes in milliseconds
+  const threeMinutes = 3 * 60 * 1000; // Three minutes in milliseconds
+  const oneMinute = 60 * 1000; // Three minutes in milliseconds
+  const oneHalfMinutes = 1.5 * 60 * 1000; // Three minutes in milliseconds
+
+  let result = Object.values(activeBookingsData).filter(booking => {
+    if (booking.vehicleType === "car") {
+      console.log(booking);
+      if (!booking.checkInTime) {
+        console.log("No check-in time for booking: ", booking);
+        return true;
+      }
+
+      // Assuming checkInTime is also a Unix timestamp in milliseconds
+      const timeDiff = currentTime - booking.checkInTime;
+
+      // return timeDiff < fiveMinutes;
+      return timeDiff < oneMinute - 2;
+    }
+  });
+
+  return result.length;
+};
+
+const getMotorcycleBookings = activeBookingsData => {
+  const currentTime = Date.now();
+  const fiveMinutes = 5 * 60 * 1000; // Three minutes in milliseconds
+  const threeMinutes = 3 * 60 * 1000; // Three minutes in milliseconds
+  const oneMinute = 60 * 1000; // Three minutes in milliseconds
+  const oneHalfMinutes = 1.5 * 60 * 1000; // Three minutes in milliseconds
+
+  let result = Object.values(activeBookingsData).filter(booking => {
+    if (booking.vehicleType === "motorcycle") {
+      console.log(booking);
+      if (!booking.checkInTime) {
+        console.log("No check-in time for booking: ", booking);
+        return true;
+      }
+
+      // Assuming checkInTime is also a Unix timestamp in milliseconds
+      const timeDiff = currentTime - booking.checkInTime;
+
+      // return timeDiff < fiveMinutes;
+      return timeDiff < oneMinute - 2;
+    }
+  });
+  return result.length;
+};
+
 const getAvailableSlots = (
   vehicleType,
   iotData,
@@ -32,18 +84,23 @@ const getAvailableSlots = (
   let emptySlots = parseInt(iotData.EMPTY);
   let halffilled = parseInt(iotData.HALF);
   let fullyfilled = parseInt(iotData.FULL);
-  let n = getActiveBookingsCount(activeBookingsData);
-  console.log("n=", n);
+  // let n = getActiveBookingsCount(activeBookingsData);
+  let carBookings = getCarBookings(activeBookingsData);
+  let motorcycleBookings = getMotorcycleBookings(activeBookingsData);
+  // console.log("n=", n);
   let availableSlotsForDisplay = 0;
   if (vehicleType === "motorcycle") {
-    availableSlotsForDisplay = (totalSlots - fullyfilled) * 2 - halffilled - n;
+    availableSlotsForDisplay =
+      (totalSlots - fullyfilled - carBookings) * 2 -
+      halffilled -
+      motorcycleBookings;
     if (availableSlotsForDisplay > 2 * totalSlots) {
       availableSlotsForDisplay = 2 * totalSlots;
     }
     return availableSlotsForDisplay < 0 ? 0 : availableSlotsForDisplay;
   }
   // for car
-  availableSlotsForDisplay = emptySlots - n;
+  availableSlotsForDisplay = emptySlots - carBookings - motorcycleBookings;
   if (availableSlotsForDisplay > totalSlots) {
     availableSlotsForDisplay = totalSlots;
   }
